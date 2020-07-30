72cdd146b34f8cfc7dcc8847f67dcd880e41fc6b
Raphael Ehindero <aph941@gmail.com>
2020-07-30 21:31:46 +0100

    chore: invoice creation page

--- invoiceApp/urls.py
+++ invoiceApp/urls.py
@@ -4,5 +4,6 @@ from invoiceApp import views
 
 urlpatterns = [
     url(r"create/$", views.invoiceCreationView, name="create_invoice"),
+    url(r"preview/$", views.invoicePreviewView, name="preview"),
     url(r"faq/$", views.faqView, name="faq"),
 ]
--- invoiceApp/views.py
+++ invoiceApp/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
+from .models import InvoiceModel
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
     return render(request, 'base.html')
 
-
 def invoiceCreationView(request):
-    return render(request, 'invoice-creation.html')
+    invoice_number = len(InvoiceModel.objects.all()) + 1
+
+    context = {
+        'invoice_number': invoice_number,
+    }
+    print(context)
+
+    return render(request, 'invoice-creation.html', context)
+
+@login_required
+def invoicePreviewView(request):
+    return render(request, 'invoice-preview.html')
 
 
 def faqView(request):

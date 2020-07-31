6e98d3ee137375687128eaec14135314fc3cb10e
prewsh <pngwube@gmail.com>
2020-07-31 10:27:32 +0100

    reset password

--- InvoiceGenerator/settings.py
+++ InvoiceGenerator/settings.py
@@ -14,6 +14,7 @@ import os
 import dj_database_url
 from decouple import config, Csv
 from django.contrib.messages import constants as messages
+from django.core.mail import send_mail
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -45,6 +46,8 @@ INSTALLED_APPS = [
     'accounts',
     'invoiceApp',
 ]
+
+
 
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -154,3 +157,12 @@ MESSAGE_TAGS = {
     messages.WARNING: 'alert-warning',
     messages.ERROR: 'alert-danger',
 }
+
+
+# SMTP CONFIGURATION
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER = '***@**.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = '*****'
--- InvoiceGenerator/urls.py
+++ InvoiceGenerator/urls.py
@@ -18,11 +18,20 @@ from django.urls import path, include
 from django.conf.urls import url
 from accounts import views as account_views
 from invoiceApp import views as invoice_views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     url(r'^$', invoice_views.home, name='home'),
     url('invoice/', include('invoiceApp.urls')),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='registration/reset_password.html'), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='registration/reset_password_sent.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_form.html'), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_complete'),
+    # path('', include('django.contrib.auth.urls')),
+
+
     #authentication urls
     url(r"signup/$", account_views.SignupView, name="signup"),
     url('^', include('django.contrib.auth.urls')),

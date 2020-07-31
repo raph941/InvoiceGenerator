1e8da99066d139abdd94bb4002879953ef3e68d3
ninyhorlah <58943705+ninyhorlah@users.noreply.github.com>
2020-07-31 13:11:07 +0100

    removed the js for downloading the pdf because it's not showing well

--- FE/assets/js/invoiceCreation.js
+++ FE/assets/js/invoiceCreation.js
@@ -142,20 +142,3 @@ function updateTotal() {
 }
 
 
-// js pdf
-var doc = new jsPDF();
-var elementHTML = $('#content').html();
-var specialElementHandlers = {
-    '#elementH': function (element, renderer) {
-        return true;
-    }
-};
-doc.fromHTML(elementHTML, 15, 15, {
-    'width': 170,
-    'elementHandlers': specialElementHandlers
-});
-
-// Save the PDF
-function savePdf() {
-    doc.save('sample-document.pdf');
-}

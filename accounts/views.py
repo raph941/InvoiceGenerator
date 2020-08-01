04cab6785ad976b693155eea49c3ff7b0f82fb31
Raphael Ehindero <aph941@gmail.com>
2020-08-01 20:51:05 +0100

    chore: Ajax Authentication completed

--- accounts/views.py
+++ accounts/views.py
@@ -33,13 +33,18 @@ def SignupAjaxView(request):
     password1 = request.POST.get('password1')
     password2 = request.POST.get('password2')
     data = {'first_name':fname, 'last_name':lname, 'email':email, 'password2':password2, 'password1':password1}
-    form = SignUpForm(data = data)
+    form = SignupForm(data)
     if form.is_valid():
         user = form.save(commit=False)
         user.is_active = True
         user.save()
         return HttpResponse(json.dumps({"message": "Success"}),content_type="application/json")
     else:
-        return HttpResponse(json.dumps({"message":form.errors}),content_type="application/json")
+        if User.objects.filter(email=email).exists():
+            return HttpResponse(json.dumps({"message":"A user with thesame Username already exists"}),content_type="application/json")
+        elif password1 and password1 != password2:
+            return HttpResponse(json.dumps({"message":"passwords did not match"}),content_type="application/json")
+        else:
+            return HttpResponse(json.dumps({"message":"An error occured, due to your provided data"}),content_type="application/json")
 	
     return HttpResponse(json.dumps({"message": "Denied"}),content_type="application/json")
--- static/js/main.js
+++ static/js/main.js
@@ -11,8 +11,9 @@ else{
 
 
 //Login
-$('#login_btn').click(function(e){
+$('#login_btn, .login_btn').click(function(e){
     e.preventDefault()
+    $('#signupModal').modal('hide')
     $('#loginModal').modal('show')
 })
 
@@ -56,19 +57,20 @@ $('#loginform').submit(function(e){
 })
 
 // Signup
-$('#signup_btn').click(function(e){
+$('#signup_btn, .signup_btn').click(function(e){
     e.preventDefault()
+    $('#loginModal').modal('hide')
     $('#signupModal').modal('show')
 })
 
-$('#signupform').submit(function(e){
+$('#sigupform').submit(function(e){
     e.preventDefault()
     var first_name     = $('#id_first_name').val()
     var last_name  = $('#id_last_name').val()
     var email  = $('#id_email').val()
     var password1  = $('#id_password1').val()
     var password2  = $('#id_password2').val()
-    var csrf  = $('#signupform').attr('csrf')
+    var csrf  = $('#sigupform').attr('csrf')
 
     $.ajax({
         url: window.location.host + "/ajax_signup/",
@@ -83,7 +85,7 @@ $('#signupform').submit(function(e){
         },
         
         success: function(data){
-            console.log(data)
+            console.log(data.message)
             if (data.message == "Success") {
                 $('#signupModal').modal('hide')
                 $('#loginModal').modal('show')

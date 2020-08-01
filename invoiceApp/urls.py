3edd86d488e244b154d7d6c8bd34dd0d8bb00864
clish254 <clishilla@gmail.com>
2020-08-01 18:00:26 +0300

    added excell sheet download feature

--- invoiceApp/urls.py
+++ invoiceApp/urls.py
@@ -5,4 +5,5 @@ from invoiceApp import views
 urlpatterns = [
     url(r"create/$", views.invoiceCreationView, name="create_invoice"),
     url(r"preview/$", views.invoicePreviewView, name="invoice_preview"),
+    url(r"excel/$", views.excelGenerationView,name="generate_excel"),
 ]
--- invoiceApp/views.py
+++ invoiceApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import InvoiceModel
 from django.contrib.auth.decorators import login_required
+from .sheet import user_invoice
+from django.http import HttpResponse
 
 
 def invoiceCreationView(request):
@@ -15,6 +17,31 @@ def invoiceCreationView(request):
     print(context)
 
     return render(request, 'invoice-creation.html', context)
+
+#view for downloading the excelsheet
+def excelGenerationView(request):
+    if(request.post):
+        invoicedata = request.POST.dict()
+        duedate = invoicedata.duedate
+        invoicedate = invoicedata.invoice_date
+        companyname = invoicedata.company_name
+        companyaddress = invoicedata.companyaddress
+        companycity = invoicedata.companycity
+        companycountry = invoicedata.companycountry
+        clientname = invoicedata.client_name
+        clientaddress = invoicedata.client_address
+        clientcountry = invoicedata.client_country
+        clientcity = invoicedata.client_city
+        invoiceitems = invoicedata.invoiceitems
+        subtotal = invoicedata.subtotal
+        salestax = invoicedata.sales_tax
+        note = invoicedata.note
+        terms = invoicedata.terms
+        user_invoice(invoicedate,duedate,companyname,companyaddress,companycity,companycountry,clientname,clientaddress,clientcity,clientcountry,invoiceitems,subtotal,salestax,note,terms)
+        file = "/static/invoice.xlsx"
+        response = HttpResponse(file,content_type='application/ms-excel')
+        response['Content-Disposition'] = 'attachment; filename="invoice.xlsx"'
+        return response
 
 @login_required
 def invoicePreviewView(request):
